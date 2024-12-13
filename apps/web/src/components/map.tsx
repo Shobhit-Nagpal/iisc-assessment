f72ac4104348949e586f7955e488ac30de4e23fe
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-14 00:33:22 +0530

    add: restrict map to blr

--- apps/web/src/components/map.tsx
+++ apps/web/src/components/map.tsx
@@ -1,16 +1,18 @@
 import { useRef } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Coordinates, MAX_BOUNDS } from "@/utils/coordinates";
 
 export function Map() {
   const mapRef = useRef(null);
-  const latitude = 51.505;
-  const longitude = -0.09;
 
   return (
     <MapContainer
-      center={[latitude, longitude]}
-      zoom={13}
+      center={Coordinates.CENTER}
+      bounds={MAX_BOUNDS}
+      maxBounds={MAX_BOUNDS}
+      zoom={11}
+      minZoom={10}
       ref={mapRef}
       style={{ height: "100vh", width: "100vw" }}
     >
---
+++ apps/web/src/utils/coordinates.ts
@@ -0,0 +1,22 @@
+import L from "leaflet";
+
+const LAT_NORTH = 13.2827;
+const LAT_SOUTH = 12.6933;
+
+const LONG_EAST = 77.0849;
+const LONG_WEST = 78.1602;
+
+export const Coordinates = {
+  NORTH: LAT_NORTH,
+  SOUTH: LAT_SOUTH,
+  EAST: LONG_EAST,
+  WEST: LONG_WEST,
+  NORTH_EAST: L.latLng(LAT_NORTH, LONG_EAST),
+  SOUTH_WEST: L.latLng(LAT_SOUTH, LONG_WEST),
+  CENTER: L.latLng(12.97, 77.56),
+};
+
+export const MAX_BOUNDS = L.latLngBounds(
+  Coordinates.SOUTH_WEST,
+  Coordinates.NORTH_EAST,
+);
